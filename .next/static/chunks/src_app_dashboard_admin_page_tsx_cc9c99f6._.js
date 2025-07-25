20d4dfb6799c8d3efcef8363eed0194ddd720de9
ardzq1 <muhammetardainci@gmail.com>
2025-07-26 01:48:56 +0300

    Add .next/static/chunks/src_app_dashboard_admin_page_tsx_cc9c99f6._.js 1753483736159

---
+++ .next/static/chunks/src_app_dashboard_admin_page_tsx_cc9c99f6._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_a4e87c90._.js",
+  "static/chunks/node_modules_82d57939._.js"
+],
+    source: "dynamic"
+});
